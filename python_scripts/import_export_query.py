4b26821260ed45696593271fe21fd67feb032664
bavagliladri <lucabrigadavilla@gmail.com>
2023-04-05 09:26:33 +0200

    add feature import export query

--- python_scripts/import_export_query.py
+++ python_scripts/import_export_query.py
@@ -1,4 +1,50 @@
 import yaml
+import wx
+
+class QueryChooser(wx.Panel):
+    """
+    Choose file panel class: shows the dialog to select the treebank file
+    """
+    def __init__(self, parent):
+        self.parent = parent
+        wx.Panel.__init__(self, parent)
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.first_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        title = wx.StaticText(self, label="Select the query file -->")
+        title.SetFont(wx.Font(10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD))
+        self.first_sizer.Add(title, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+        openFileDlgBtn = wx.Button(self, label='Choose file')
+        openFileDlgBtn.Bind(wx.EVT_BUTTON, self.onOpenQuery)
+        self.first_sizer.Add(openFileDlgBtn, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+        self.main_sizer.Add(self.first_sizer)
+        self.SetSizer(self.main_sizer)
+
+    def onOpenQuery(self, event):
+        """
+        Create and show the Open FileDialog
+        """
+        wildcard = "YAML file (*.yaml;*.yml;*.YAML;*.YML)|*.yaml;*.yml;*.YAML;*.YML|" \
+                   "All files (*.*)|*.*"
+
+        dlg = wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir="",
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.FD_OPEN | wx.FD_MULTIPLE | wx.FD_CHANGE_DIR
+        )
+        if dlg.ShowModal() == wx.ID_OK:
+            paths = dlg.GetPaths()
+            setattr(self, 'query_path', paths[0])
+        dlg.Destroy()
+        if hasattr(self, "selected_file"):
+            self.selected_file.Destroy()
+        setattr(self, "second_sizer", wx.BoxSizer(wx.HORIZONTAL))
+        setattr(self, "selected_file", wx.StaticText(self, label=f"Selected file: {getattr(self, 'query_path')}"))
+        self.second_sizer.Add(self.selected_file, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+        self.main_sizer.Add(self.second_sizer, 0, wx.EXPAND | wx.ALL, 5)
+        self.SetSizer(self.main_sizer)
+        self.Parent.Layout()
 
 
 def export_q(data, dest):
